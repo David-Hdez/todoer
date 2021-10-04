43dd688a94d996c19b190eed19f4a011265bd54d
David Hernandez <54561496+David-Hdez@users.noreply.github.com>
2021-10-03 21:12:47 -0500

    Inicio de sesión
    Login and register, validation and in case returning template

--- todo/auth.py
+++ todo/auth.py
@@ -35,8 +35,34 @@ def register():
                 (username, generate_password_hash(password)),
             )
             db.commit()
+
             return redirect(url_for("auth.login"))
 
         flash(error)
 
-    return render_template('auth/register.html')
+
+@bp.route('/login', methods=('GET', 'POST'))
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db, c = get_db()
+        error = None
+        c.execute(
+            'SELECT * FROM user WHERE username = %s', (username,)
+        )
+        user = c.fetchone()
+
+        if user is None:
+            error = 'Incorrect username and/or password.'
+        elif not check_password_hash(user['password'], password):
+            error = 'Incorrect username and/or password..'
+
+        if error is None:
+            session.clear()
+            session['user_id'] = user['id']
+            return redirect(url_for('index'))
+
+        flash(error)
+
+    return render_template('auth/login.html')
