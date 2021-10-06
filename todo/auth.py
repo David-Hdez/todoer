0e03ac835135d47c5d126891d219527d215edd9e
David Hernandez <54561496+David-Hdez@users.noreply.github.com>
2021-10-05 22:19:37 -0500

    Listando Todos
    Vista index cargando registros.

--- todo/auth.py
+++ todo/auth.py
@@ -63,7 +63,7 @@ def login():
         if error is None:
             session.clear()
             session['user_id'] = user['id']
-            return redirect(url_for('index'))
+            return redirect(url_for('todo.index'))
 
         flash(error)
 
